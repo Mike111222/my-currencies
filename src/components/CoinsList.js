2c94a291009a3ff97482d46025a1d05dc4579f70
Mike111222 <Tipu870@gmail.com>
2023-08-01 11:06:49 +0500

    Update CoinsList.js

--- src/components/CoinsList.js
+++ src/components/CoinsList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import arrowRight from '../arrowRight.svg';
 import { fetchCoins, searchCoins } from '../Redux/coin/coinSlice';
 import { setCoinDetail } from '../Redux/detail/detailSlice';
 
@@ -21,8 +22,14 @@ const CoinsList = () => {
     dispatch(searchCoins(searchQuery));
   };
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   if (loading) {
-    return <div>Loading...</div>;
+    return <div className="whiteColor loading">Loading...</div>;
   }
 
   if (error) {
@@ -37,34 +44,45 @@ const CoinsList = () => {
 
   return (
     <div>
-      <h2>Coins</h2>
-      <div>
+      <div><arrowRightCircle /></div>
+      <div className="searchContainer">
+        <h2 className="whiteColor">Search Crypto Here</h2>
         <input
+          className="searchBar"
           type="text"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyDown={handleKeyDown}
+          placeholder="Search by name"
         />
-        <button type="button" onClick={handleSearch}>
+        <button className="searchBtn" type="button" onClick={handleSearch}>
           Search
         </button>
       </div>
-      {coins.map((coin) => (
-        <div key={coin.id}>
-          <NavLink to="/detail" onClick={() => handleCoinClick(coin)}>
-            <h3>{coin.name}</h3>
-            <p>
-              Symbol:
-              {' '}
-              {coin.symbol}
-            </p>
-            <p>
-              Price:
-              {' '}
-              {coin.priceUsd}
-            </p>
-          </NavLink>
-        </div>
-      ))}
+      <div className="coinsList">
+        {coins.map((coin) => (
+          <div key={coin.id} className="coinCard">
+            <NavLink to="/detail" onClick={() => handleCoinClick(coin)}>
+              <div className="rightArrow">
+                <h2>
+                  {coin.name}
+                </h2>
+                <img className="rightArrowImg" src={arrowRight} alt="right arrow" />
+              </div>
+              <p className="text">
+                Symbol:
+                {' '}
+                {coin.symbol}
+              </p>
+              <p className="text">
+                Price:
+                {' '}
+                {typeof coin.priceUsd === 'number' ? coin.priceUsd.toFixed(2) : parseFloat(coin.priceUsd).toFixed(2)}
+              </p>
+            </NavLink>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
